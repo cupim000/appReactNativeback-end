8e17ff9c3dc66643d36e76a8671e2502a34c5a30
Lucas <lucas.mendes@vizir.com.br>
2021-03-21 20:15:16 -0300

    try new env

---
+++ ormconfig.js
@@ -0,0 +1,34 @@
+module.exports ={
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "entities": [
+    "dist/models/**/*.js"
+ ],
+ "migrations": [
+  "dist/database/migrations/**/*.js"
+],
+ "cli":{
+  "migrationsDir": [
+    "src/database/migrations/"
+  ],
+  "entitiesDir": "src/models"
+  }
+
+}
+// {
+//   "type": "postgres",
+//   "host": "localhost",
+//   "port": 5432,
+//   "username": "postgres",
+//   "password": "docker",
+//   "database": "appReact",
+//   "entities": [
+//     "./src/models/*.ts"
+//   ],
+//   "migrations": [
+//     "./src/database/migrations/*.ts"
+//   ],
+//   "cli":{
+//     "migrationsDir": "./src/database/migrations"
+//   }
+// }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes';
 
 import './database';
 
 const app = express();
+const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Server Online');
+dotenv.config();
+
+app.listen(PORT, () => {
+  console.log(`🏃 Server Online on port ${PORT}`);
 });
