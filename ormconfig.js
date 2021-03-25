edb09a7930f017c23a7357e2fd821662d570c4fb
Lucas <lucas.mendes@vizir.com.br>
2021-03-24 23:34:25 -0300

    migration tretas

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,5 @@
-module.exports ={
+console.log('process.env.DATABASEE_URL :>> ', process.env.DATABASE_URL);
+module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "entities": [
@@ -13,9 +14,7 @@ module.exports ={
   ],
   "entitiesDir": "src/models"
   },
-  "extra": {
-    "ssl": "true"
-}
+  "ssl":"true"
 }
 
 
