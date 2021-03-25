e9d5d10b7358d81e68d3e42ab029d77fc56133a4
Lucas <lucas.mendes@vizir.com.br>
2021-03-24 23:11:39 -0300

    migration tretas

--- ormconfig.js
+++ ormconfig.js
@@ -1,34 +1,36 @@
-module.exports ={
-  "type": "postgres",
-  "url": process.env.DATABASE_URL,
-  "entities": [
-    "dist/models/**/*.js"
- ],
- "migrations": [
-  "dist/database/migrations/**/*.js"
-],
- "cli":{
-  "migrationsDir": [
-    "src/database/migrations/"
-  ],
-  "entitiesDir": "src/models"
-  }
-
-}
 // module.exports ={
 //   "type": "postgres",
-//   "host": "localhost",
-//   "port": 5432,
-//   "username": "postgres",
-//   "password": "docker",
-//   "database": "appReact",
+//   "url": process.env.DATABASE_URL,
 //   "entities": [
-//     "./src/models/*.ts"
+//     "dist/models/**/*.js"
+//  ],
+//  "migrations": [
+//   "dist/database/migrations/**/*.js"
+// ],
+//  "cli":{
+//   "migrationsDir": [
+//     "src/database/migrations/"
 //   ],
-//   "migrations": [
-//     "./src/database/migrations/*.ts"
-//   ],
-//   "cli":{
-//     "migrationsDir": "./src/database/migrations"
+//   "entitiesDir": "src/models"
 //   }
+
 // }
+
+
+module.exports ={
+  "type": "postgres",
+  "host": "localhost",
+  "port": 5432,
+  "username": "postgres",
+  "password": "docker",
+  "database": "appReact",
+  "entities": [
+    "./src/models/*.ts"
+  ],
+  "migrations": [
+    "./src/database/migrations/*.ts"
+  ],
+  "cli":{
+    "migrationsDir": "./src/database/migrations"
+  }
+}
--- src/database/migrations/1616638274812-CreateUser.ts
+++ src/database/migrations/1616638274812-CreateUser.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateUser1616118702144 implements MigrationInterface {
-    name = 'CreateUser1616118702144'
+export class CreateUser1616638274812 implements MigrationInterface {
+    name = 'CreateUser1616638274812'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "companies" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "organizationNumber" character varying NOT NULL, "address" character varying NOT NULL, "contact" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_d4bc3e82a314fa9e29f652c2c22" PRIMARY KEY ("id"))`);
